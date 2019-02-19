bbb437cb90cac48d0162b777bd4aed83a9598c0f
christianhauff <24371132+christianhauff@users.noreply.github.com>
2019-02-19 16:15:51 +0100

    Added Ansible Files for creating a Replica Set without Sharding

---
+++ start_router.sh
@@ -0,0 +1,4 @@
+sudo iptables -t nat -A POSTROUTING -o wlan0 -j MASQUERADE
+sudo iptables -A FORWARD -i wlan0 -o eth0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+sudo iptables -A FORWARD -i eth0 -o wlan0 -j ACCEPT
+
