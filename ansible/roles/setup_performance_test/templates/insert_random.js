8d427af8ff29c022ff02e4ce4a290faea0ca1ccb
christianhauff <24371132+christianhauff@users.noreply.github.com>
2019-07-25 09:24:29 +0200

    reduced number of performance test inserts

--- ansible/roles/setup_performance_test/templates/insert_random.js
+++ ansible/roles/setup_performance_test/templates/insert_random.js
@@ -2,4 +2,4 @@ function rand() {return Math.round(Math.random()*1000000)};
 
 db = db.getSiblingDB('testdb');
 
-for (i=0;i<200000;i++){db.testCol.insert({key:rand(),val1:rand(),val2:rand()});}
+for (i=0;i<20000;i++){db.testCol.insert({key:rand(),val1:rand(),val2:rand()});}
