0bba8f6e222543d80e10c878b555b24b4f7c38d3
christianhauff <24371132+christianhauff@users.noreply.github.com>
2019-07-25 10:43:24 +0200

    refactoring of start_router.sh to cover all necessary settings

--- start_router.sh
+++ start_router.sh
@@ -1,4 +1,49 @@
-sudo iptables -t nat -A POSTROUTING -o wlan0 -j MASQUERADE
-sudo iptables -A FORWARD -i wlan0 -o eth0 -m state --state RELATED,ESTABLISHED -j ACCEPT
-sudo iptables -A FORWARD -i eth0 -o wlan0 -j ACCEPT
+#!/bin/bash
 
+######      Interface configuration      ######
+
+iface_pub="wlan0"     #interface having connection to the public internet
+iface_cluster="eth0"  #interface connected to the cluster
+
+### Don't change anything below this point! ###
+
+ipforward=$(cat /proc/sys/net/ipv4/ip_forward)
+
+if [ $ipforward -ne '1' ];
+then
+printf "ipv4 forwarding has to be activated. This is done by the command \
+'sysctl -w net.ipv4.ip_forward=1'. Do you want to execute it now? (y/n) "
+read execconfirm
+
+  if [ "$execconfirm" == "y" ];
+  then
+    sysctl -w net.ipv4.ip_forward=1
+  else
+    echo "You have to activate ipv4 forwarding manually before continuing."
+    exit 0
+  fi
+
+else
+echo "ipv4 forwarding is activated correctly"
+fi
+
+printf "Is it correct that your network interface ${iface_pub} is connected \
+to the public internet and the interface ${iface_cluster} is connected to the \
+cluster? (y/n) "
+read ifaceconfirm
+
+if [ "$ifaceconfirm" != "y" ];
+then
+echo "Please edit the variables in the top lines of this script to fit \
+your network configuration"
+exit 0
+fi
+
+echo "Applying iptables rules"
+
+sudo iptables -t nat -A POSTROUTING -o ${iface_pub} -j MASQUERADE
+sudo iptables -A FORWARD -i ${iface_pub} -o ${iface_cluster} -m state --state RELATED,ESTABLISHED -j ACCEPT
+sudo iptables -A FORWARD -i ${iface_cluster} -o ${iface_pub} -j ACCEPT
+
+echo "Done! This computer is working as a router for the Cluster now!"
+exit 0
